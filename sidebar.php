a97ff7f465d6755d98d364dd108942d2e9721ecd
Michael Sisk <contact@mgsisk.com>
2011-09-25 09:49:56 -0400

    Correct <figcapture> to <figcaption>.
    This was addressed in a previous commit, but rollback attempts
    apparently removed the update.

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
 	<div>
 	<?php if ( !dynamic_sidebar( 'inkblot-sidebar1' ) ) { //see functions.php hook_init ?>
 		<figure>
-			<figcapture><?php _e( 'Sidebar 1', 'inkblot' ); ?></figcapture>
+			<figcaption><?php _e( 'Sidebar 1', 'inkblot' ); ?></figcaption>
 			<?php _e( 'This area is widgetized. You can add widgets by going to <em>Appearance > Widgets</em> in the administrative dashboard. Adding widgets will remove this message.', 'inkblot' ); ?>
 		</figure>
 	<?php } ?>
@@ -13,7 +13,7 @@
 	<div>
 	<?php if ( !dynamic_sidebar( 'inkblot-sidebar2' ) ) { //see functions.php hook_init ?>
 		<figure>
-			<figcapture><?php _e( 'Sidebar 2', 'inkblot' ); ?></figcapture>
+			<figcaption><?php _e( 'Sidebar 2', 'inkblot' ); ?></figcaption>
 			<?php _e( 'This area is widgetized. You can add widgets by going to <em>Appearance > Widgets</em> in the administrative dashboard. Adding widgets will remove this message.', 'inkblot' ); ?>
 		</figure>
 	<?php } ?>
