c1689d6d321e9662848a94cde8556c55c7e8b629
Michael Sisk <contact@mgsisk.com>
2013-02-17 07:50:46 -0500

    Add data-webcomic-getures support.

--- webcomic/home.php
+++ webcomic/home.php
@@ -1,5 +1,5 @@
 <?php
-/** Webcomic index template.
+/** Webcomic home template.
  * 
  * This is the Webcomic-specific homepage template (used in place of
  * the normal `home.php` or `index.php` when Webcomic is active).
@@ -24,21 +24,19 @@ get_header(); ?>
 
 <?php if ( !get_theme_mod( 'webcomic_content', false ) and $webcomics and $webcomics->have_posts() ) : ?>
 	<?php while ( $webcomics->have_posts() ) : $webcomics->the_post(); ?>
-		<div id="webcomic" class="post-webcomic" data-webcomic-shortcuts data-webcomic-container>
+		<div id="webcomic" class="post-webcomic" data-webcomic-shortcuts data-webcomic-gestures data-webcomic-container>
 			<?php get_template_part( 'webcomic/webcomic', get_post_type() ); ?>
 		</div><!-- .post-webcomic -->
 	<?php endwhile; $webcomics->rewind_posts(); ?>
 <?php endif; ?>
 <main role="main">
-	<?php if ( get_theme_mod( 'webcomic_content', false ) and $webcomics and $webcomics->have_posts() ) : ?>
-		<?php while ( $webcomics->have_posts() ) : $webcomics->the_post(); ?>
-			<div id="webcomic" class="post-webcomic" data-webcomic-shortcuts data-webcomic-container>
-				<?php get_template_part( 'webcomic/webcomic', get_post_type() ); ?>
-			</div><!-- .post-webcomic -->
-		<?php endwhile; $webcomics->rewind_posts(); ?>
-	<?php endif; ?>
 	<?php if ( $webcomics and $webcomics->have_posts() ) : ?>
 		<?php while ( $webcomics->have_posts() ) : $webcomics->the_post(); ?>
+			<?php if ( get_theme_mod( 'webcomic_content', false ) ) : ?>
+				<div id="webcomic" class="post-webcomic" data-webcomic-shortcuts data-webcomic-gestures data-webcomic-container>
+					<?php get_template_part( 'webcomic/webcomic', get_post_type() ); ?>
+				</div><!-- .post-webcomic -->
+			<?php endif; ?>
 			<?php get_template_part( 'webcomic/content', get_post_type() ); ?>
 		<?php endwhile; ?>
 	<?php elseif ( $webcomics and current_user_can( 'edit_posts' ) ) : ?>
--- webcomic/single.php
+++ webcomic/single.php
@@ -13,13 +13,13 @@ get_header(); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 	<?php if ( !get_theme_mod( 'webcomic_content', false ) ) : ?>
-		<div id="webcomic" class="post-webcomic" data-webcomic-shortcuts>
+		<div id="webcomic" class="post-webcomic" data-webcomic-shortcuts data-webcomic-gestures>
 			<?php get_template_part( 'webcomic/webcomic', get_post_type() ); ?>
 		</div><!-- .post-webcomic -->
 	<?php endif; ?>
 	<main role="main">
 		<?php if ( get_theme_mod( 'webcomic_content', false ) ) : ?>
-			<div id="webcomic" class="post-webcomic" data-webcomic-shortcuts>
+			<div id="webcomic" class="post-webcomic" data-webcomic-shortcuts data-webcomic-gestures>
 				<?php get_template_part( 'webcomic/webcomic', get_post_type() ); ?>
 			</div><!-- .post-webcomic -->
 		<?php endif; ?>
