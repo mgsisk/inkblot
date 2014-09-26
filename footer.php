a6a9255eccbfa8a91c8538641447a815ea6579d0
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:10:46 -0400

    Refactor footer.php template.

--- footer.php
+++ footer.php
@@ -1,23 +1,31 @@
 <?php
-/** Footer template.
+/**
+ * Footer template.
  * 
  * @package Inkblot
  */
 ?>
-				<div id="content-footer" role="complementary" class="widgets"><?php dynamic_sidebar( 'content-footer' ); ?></div><!-- #content-footer -->
+				<?php print inkblot_widgetized('content-footer'); ?>
 			</div><!-- #content -->
 			<footer id="footer" role="contentinfo">
+				
 				<?php
-					printf( __( '<a href="#document">%1$s</a> &bull; Powered by %2$s with %3$s', 'inkblot' ),
-						InkblotTag::inkblot_copyright(),
+					printf(__('%1$s &bull; Powered by %2$s with %3$s', 'inkblot'),
+						'<a href="#document">' . inkblot_copyright() . '</a>',
 						'<a href="//wordpress.org" target="_blank">WordPress</a>',
 						'<a href="//github.com/mgsisk/inkblot" target="_blank">Inkblot</a>'
 					);
 				?>
+				
 			</footer><!-- #contentinfo -->
-			<div id="page-footer" role="complementary" class="widgets"><?php dynamic_sidebar( 'page-footer' ); ?></div><!-- #page-footer -->
+			<?php print inkblot_widgetized('page-footer'); ?>
 		</div><!-- #page -->
-		<div id="document-footer" role="complementary" class="widgets"><?php dynamic_sidebar( 'document-footer' ); ?></div><!-- #document-footer -->
-		<?php wp_footer(); ?>
+		
+		<?php
+			print inkblot_widgetized('document-footer');
+			
+			wp_footer();
+		?>
+		
 	</body><!-- #document -->
 </html>
