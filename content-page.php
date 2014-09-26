456250b0f9b3c31576b157de56f81e8f2f459d4c
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:11:15 -0400

    Refactor page content template.

--- content-page.php
+++ content-page.php
@@ -1,24 +1,42 @@
 <?php
-/** Page content template.
+/**
+ * Page content template.
  * 
  * @package Inkblot
  */
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( has_post_thumbnail() ) : ?>
-		<div class="post-image">
-			<?php the_post_thumbnail(); ?>
-		</div><!-- .post-image -->
+	
+	<?php if (has_post_thumbnail()) : ?>
+		<div class="post-image"><?php the_post_thumbnail(); ?></div><!-- .post-image -->
 	<?php endif; ?>
+	
 	<header class="post-header">
 		<h1><?php the_title(); ?></h1>
+		<div class="post-details">
+			
+			<?php
+				if ( ! post_password_required() and (comments_open() or get_comments_number())) :
+					print comments_popup_link();
+				endif;
+				
+				edit_post_link();
+			?>
+			
+		</div>
 	</header><!-- .post-header -->
 	<div class="post-content">
-		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<nav class="post-pages">', 'after' => '</nav>' ) ); ?>
-	</div><!-- .post-content -->
-	<footer class="post-footer">
-		<?php inkblot_post_meta(); ?>
-	</footer><!-- .post-footer -->
+		
+		<?php
+			the_content();
+			
+			wp_link_pages(array(
+				'before' => '<nav class="post-pages">',
+				'after' => '</nav>'
+			));
+		?>
+		
+	</div>
+	
 </article><!-- #post-<?php the_ID(); ?> -->
