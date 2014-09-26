df75182c8d55551135fc6c41b56fcf31d3859df7
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:09:23 -0400

    Refactor page template.

--- page.php
+++ page.php
@@ -1,17 +1,28 @@
 <?php
-/** Generic page template.
+/**
+ * Generic page template.
+ * 
+ * For Webcomic page templates, see `webcomic/template-archive.php` or
+ * `webcomic/template-home.php`.
  * 
  * @package Inkblot
+ * @see http://codex.wordpress.org/Template_Hierarchy
  */
 
 get_header(); ?>
 
 <main role="main">
-	<?php while ( have_posts() ) : the_post(); ?>
-		<?php get_template_part( 'content', 'page' ); ?>
-		<?php comments_template( '', true ); ?>
-	<?php endwhile;?>
+	
+	<?php
+		while (have_posts()) :
+			the_post();
+			
+			get_template_part('content', 'page');
+			
+			comments_template();
+		endwhile;
+	?>
+	
 </main>
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_sidebar(); get_footer();
