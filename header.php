9bc6e3c89f7052ff6a9f2a756953fa7892277cde
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:12:15 -0400

    Refactor header template.

--- header.php
+++ header.php
@@ -1,40 +1,61 @@
 <?php
-/** Header template.
+/**
+ * Header template.
  * 
  * @package Inkblot
+ * @see http://codex.wordpress.org/Template_Hierarchy
  */
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
-	<head><?php wp_head(); ?></head>
+	<head><?php wp_head(); /* @see `inkblot_wp_head()` in `functions.php` */ ?></head>
 	<body id="document" <?php body_class(); ?>>
-		<div id="document-header" role="complementary" class="widgets"><?php dynamic_sidebar( 'document-header' ); ?></div><!-- #document-header -->
+		<a href="#content">Skip to content</a>
+		<?php print inkblot_widgetized('document-header'); ?>
 		<div id="page">
-			<div id="page-header" role="complementary" class="widgets"><?php dynamic_sidebar( 'page-header' ); ?></div><!-- #page-header -->
+			<?php print inkblot_widgetized('page-header'); ?>
 			<header id="header" role="banner">
-				<hgroup>
-					<h1><a href="<?php echo esc_url( home_url() ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<h2><?php bloginfo( 'description' ); ?></h2>
-				</hgroup>
-				<?php if ( $header = get_custom_header() and $header->url ) : ?>
-					<a href="<?php echo esc_url( home_url() ); ?>" rel="home"><img src="<?php header_image(); ?>" width="<?php echo $header->width; ?>" height="<?php echo $header->height; ?>" alt=""></a>
-				<?php endif; ?>
+				<a href="<?php print esc_url(home_url()); ?>" rel="home">
+					<h1><?php bloginfo('name'); ?></h1>
+					<p><?php bloginfo('description'); ?></p>
+					
+					<?php if ($header = get_custom_header() and $header->url) : ?>
+						<img src="<?php header_image(); ?>" width="<?php print $header->width; ?>" height="<?php print $header->height; ?>" alt="">
+					<?php endif; ?>
+					
+				</a>
 				<nav>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'show_home' => true, 'container' => false ) ); ?>
+					
 					<?php
-						if ( get_theme_mod( 'responsive', true ) or inkblot_theme_preview() ) {
-							if ( has_nav_menu( 'primary' ) ) {
-								wp_nav_menu( array( 'theme_location' => 'primary', 'show_home' => true, 'container' => false, 'items_wrap' => '<select>%3$s</select>', 'walker' => new Walker_InkblotNavMenu_Dropdown ) );
+						wp_nav_menu(array(
+							'theme_location' => 'primary',
+							'show_home' => true,
+							'container' => false
+						));
+						
+						if (get_theme_mod('responsive_width', 0) or is_customize_preview()) {
+							if (has_nav_menu('primary')) {
+								wp_nav_menu(array(
+									'theme_location' => 'primary',
+									'show_home' => true,
+									'container' => false,
+									'items_wrap' => '<select>%3$s</select>',
+									'walker' => new Inkblot_Walker_Menu_Dropdown
+								));
 							} else {
-								echo '<select>';
+								print '<select>';
 								
-								wp_list_pages( array( 'title_li' => '', 'walker' => new Walker_InkblotPageMenu_Dropdown ) );
+								wp_list_pages(array(
+									'title_li' => '',
+									'walker' => new Inkblot_Walker_Page_Dropdown
+								));
 								
-								echo '</select>';
+								print '</select>';
 							}
 						}
 					?>
+					
 				</nav>
 			</header><!-- #header -->
 			<div id="content">
-				<div id="content-header" role="complementary" class="widgets"><?php dynamic_sidebar( 'content-header' ); ?></div><!-- #content-header -->
+				<?php print inkblot_widgetized('content-header'); ?>
