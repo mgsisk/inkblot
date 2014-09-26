230e4d7e63e70208b2baac1aa690e8a9704d1753
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:13:49 -0400

    Add contributors template.
    Great for creating a site contributors page!

---
+++ page-templates/contributors.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template Name: Contributors
+ * 
+ * Contributors page template.
+ * 
+ * @package Inkblot
+ * @see http://codex.wordpress.org/Template_Hierarchy
+ */
+
+get_header(); ?>
+
+<main role="main">
+
+	<?php
+		while (have_posts()) :
+			the_post();
+			
+			get_template_part('content', 'page');
+			
+			$users = get_users(array(
+				'fields' => 'ID',
+				'orderby' => 'post_count',
+				'order' => 'DESC',
+				'who' => 'authors'
+			));
+			
+			if ($users) :
+				foreach ($users as $user) :
+					print inkblot_contributor($user, get_post_meta('inkblot_avatar'));
+				endforeach;
+			endif;
+			
+			comments_template();
+		endwhile;
+	?>
+
+</main>
+
+<?php get_sidebar(); get_footer();
