a4abd9dc9b06198bb7dc0f517560461664d1f813
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:12:27 -0400

    Refactor no content template.

--- content-none.php
+++ content-none.php
@@ -1,13 +1,37 @@
 <?php
-/** Generic no content template.
+/**
+ * No content template.
  * 
  * @package Inkblot
  */
 ?>
-<header class="page-header">
-	<h1><?php _e( 'Nothing Found', 'inkblot' ); ?></h1>
-</header><!-- .page-header -->
-<div class="page-content">
-	<p><?php _e( 'Nothing could be found for the requested archive.', 'inkblot' ); ?></p>
-	<?php get_search_form(); ?>
+
+<?php if ( ! is_home()) : ?>
+	
+	<header class="post-header">
+		<h1><?php _e('Nothing Found', 'inkblot'); ?></h1>
+	</header><!-- .page-header -->
+	
+<?php endif; ?>
+
+<div class="post-content">
+	<p>
+		
+		<?php
+			if (is_home() and current_user_can('publish_posts')) :
+				printf('<a href="%s" class="first-post">%s</a>',
+					admin_url('post-new.php'),
+					__('Publish your first post', 'inkblot')
+				);
+			elseif (is_search()) :
+				_e('Sorry, but nothing matched your search terms. Please try again with different keywords.', 'inkblot');
+			elseif (!is_home()) :
+				_e("Apologies, but we can't seem to find what you're looking for. Perhaps searching will help.", 'inkblot');
+			endif;
+		?>
+		
+	</p>
+	
+	<?php is_home() ? '' : get_search_form(); ?>
+
 </div><!-- .page-content -->
