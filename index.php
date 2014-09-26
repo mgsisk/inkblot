576613216867b991faee3c7ed516ad3ed685e3a5
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:10:00 -0400

    Refactor main template file.

--- index.php
+++ index.php
@@ -1,32 +1,31 @@
 <?php
-/** The main template file.
+/**
+ * The main template file.
  * 
- * If you're using Webcomic you'll want to look at
- * /webcomic/index.php
+ * If you're using Webcomic you'll want to look at `webcomic/home.php`.
  * 
  * @package Inkblot
+ * @see http://codex.wordpress.org/Template_Hierarchy
  */
 
 get_header(); ?>
 
 <main role="main">
-	<?php if ( have_posts() ) : ?>
-		<?php inkblot_posts_nav( 'above' ); ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
-		<?php endwhile; ?>
-		<?php inkblot_posts_nav( 'below' ); ?>
-	<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
-		<header class="page-header">
-			<h1><?php _e( 'No Posts', 'inkblot' ); ?></h1>
-		</header><!-- .page-header -->
-		<div class="page-content">
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%s">Start here &raquo;</a>', 'inkblot' ), admin_url( 'post-new.php' ) ); ?></p>
-		</div><!-- .page-content -->
-	<?php else : ?>
-		<?php get_template_part( 'content', 'none' ); ?>
-	<?php endif; ?>
+	
+	<?php
+		if (have_posts()) :
+			while (have_posts()) :
+				the_post();
+				
+				get_template_part('content', get_post_format());
+			endwhile;
+			
+			print inkblot_posts_nav('below', false, get_theme_mod('paged_navigation', true));
+		else :
+			get_template_part('content', 'none');
+		endif;
+	?>
+	
 </main>
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_sidebar(); get_footer();
