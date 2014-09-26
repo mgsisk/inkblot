47625db32b13f6fa273ca23e267167f9ccb442fd
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:12:35 -0400

    Refactor comments template.

--- comments.php
+++ comments.php
@@ -1,33 +1,43 @@
 <?php
-/** Comments template.
+/**
+ * Comments template.
  * 
  * @package Inkblot
+ * @see http://codex.wordpress.org/Template_Hierarchy
  */
 ?>
 
 <aside id="comments">
-	<div id="comment-header" role="complementary" class="widgets"><?php dynamic_sidebar( 'comment-header' ); ?></div><!-- #comment-header -->
-	<?php if ( post_password_required() ) : ?>
+	
+	<?php
+		print inkblot_widgetized('comment-header');
 		
-	<?php else : ?>
-		<?php if ( have_comments() ) : ?>
-			<header class="comments-header">
-				<h1><?php comments_number( '', __( 'One Comment', 'inkblot' ), __( '% Comments', 'inkblot' ) ); ?></h1>
-			</header><!-- .comments-header -->
-			<?php inkblot_comments_nav( 'above' ); ?>
+		if (post_password_required()) :
+			// a password is required to view this post
+		else :
+			if (have_comments()) : ?>
+				
+				<header class="comments-header">
+					<h1><?php comments_number(); ?></h1>
+				</header><!-- .comments-header -->
+				
 			<?php
-				wp_list_comments( array(
-					'style'        => 'div',
-					'avatar_size'  => 32,
-					'callback'     => 'inkblot_start_comment',
+				wp_list_comments(array(
+					'style' => 'div',
+					'avatar_size' => 64,
+					'callback' => 'inkblot_start_comment',
 					'end-callback' => 'inkblot_end_comment'
-				) );
-			?>
-			<?php inkblot_comments_nav( 'below' ); ?>
-		<?php elseif ( !comments_open() and get_comments_number() and post_type_supports( get_post_type(), 'comments' ) ) : ?>
+				));
+				
+				print inkblot_comments_nav('below', get_theme_mod('paged_comments', false));
+			elseif ( ! comments_open() and get_comments_number() and post_type_supports(get_post_type(), 'comments')) :
+				// comments are closed, but the post has and supports comments
+			endif;
 			
-		<?php endif; ?>
-		<?php comment_form(); ?>
-	<?php endif; ?>
-	<div id="comment-footer" role="complementary" class="widgets"><?php dynamic_sidebar( 'comment-footer' ); ?></div><!-- #comment-footer -->
+			comment_form();
+		endif;
+		
+		print inkblot_widgetized('comment-footer');
+	?>
+	
 </aside><!-- #comments -->
