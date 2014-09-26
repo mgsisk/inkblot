d7f355d1e8f7e6a290dfc27458de80f01108963d
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:13:22 -0400

    Refactor admin page script.
    Accounts for the new location of page templates, as well as the two new
    templates (contributors and full-width).

--- -/js/admin-pages.js
+++ -/js/admin-pages.js
@@ -1,15 +1,25 @@
-/** Handle dynamic template options display.
+/**
+ * Handle dynamic template options display.
  * 
  * @package Inkblot
  */
-jQuery( function( $ ) {
-	$( '#page_template' ).on( 'change', function() {
-		$( '#inkblot-options .inside > div' ).hide();
+jQuery(function($) {
+	$('#inkblot-template-options h3').append('<span class="inkblot-template-title"></span>');
+	
+	$('#page_template').on('change', function() {
+		$('[data-inkblot-template-options], [data-inkblot-template-options] h4').hide();
 		
-		if ( -1 !== $( this ).val().indexOf( 'webcomic/' ) ) {
-			$( '[data-inkblot-options="' + $( this ).val() + '"]' ).show();
+		if (-1 !== $.inArray($(this).val(), [
+			'page-templates/contributors.php',
+			'page-templates/full-width.php',
+			'page-templates/webcomic-archive.php',
+			'page-templates/webcomic-homepage.php'
+		])) {
+			$('#inkblot-template-options .inkblot-template-title').text(' - ' + $('[data-inkblot-template-options="' + $(this).val() + '"] h4').text());
+			$('[data-inkblot-template-options="' + $(this).val() + '"]').show();
 		} else {
-			$( '[data-inkblot-options="none"]' ).show();
+			$('#inkblot-template-options h3 .inkblot-template-title').text('');
+			$('[data-inkblot-template-options="none"]').show();
 		}
-	} ).trigger( 'change' );
-} );
+	}).trigger('change');
+});
