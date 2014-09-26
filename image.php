16930a637f2a28cf8048c24254a1efaa7e2e2530
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:10:16 -0400

    Refactor image template.

--- image.php
+++ image.php
@@ -1,40 +1,73 @@
 <?php
-/** Image attachment template.
+/**
+ * Image attachment template.
  * 
  * @package Inkblot
+ * @see http://codex.wordpress.org/Template_Hierarchy
  */
 
 get_header(); ?>
 
 <main role="main">
-	<?php while ( have_posts() ) : the_post(); ?>
+	
+	<?php while (have_posts()) : the_post(); ?>
+		
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<header class="post-header">
 				<h1><?php the_title(); ?></h1>
+				<div class="post-details">
+					
+					<?php
+						print inkblot_post_parent();
+						
+						print inkblot_image_details();
+						
+						if ( ! post_password_required() and (comments_open() or get_comments_number())) :
+							comments_popup_link();
+						endif;
+						
+						edit_post_link();
+					?>
+					
+				</div>
 			</header><!-- .post-header -->
-			<footer class="post-footer">
-				<?php inkblot_post_meta(); ?>
-			</footer><!-- .post-footer -->
+			
 			<nav class="posts">
-				<?php previous_image_link( array( 32, 32 ) ); ?>
-				<?php next_image_link( array( 32, 32 ) ); ?>
+				
+				<?php
+					previous_image_link(array(64, 64));
+					next_image_link(array(64, 64));
+				?>
+				
 			</nav>
+			
 			<div class="post-image">
-				<?php the_attachment_link( $post->ID, true ); ?>
-			</div><!-- .post-attachment -->
-			<?php if ( $post->post_excerpt ) : ?>
-				<div class="post-excerpt">
-					<?php the_excerpt(); ?>
-				</div><!-- .post-excerpt -->
+				<?php the_attachment_link($post->ID, true); ?>
+			</div><!-- .post-image -->
+			
+			<?php if ($post->post_excerpt) : ?>
+				<div class="post-excerpt"><?php the_excerpt(); ?></div><!-- .post-excerpt -->
 			<?php endif; ?>
+			
 			<div class="post-content">
-				<?php the_content(); ?>
-				<?php wp_link_pages( array( 'before' => '<nav class="post-pages">', 'after' => '</nav>' ) ); ?>
+				
+				<?php
+					the_content();
+					
+					wp_link_pages(array(
+						'before' => '<nav class="post-pages">',
+						'after' => '</nav>'
+					));
+				?>
+				
 			</div><!-- .post-content -->
+			
 		</article><!-- #post-<?php the_ID(); ?> -->
-		<?php comments_template( '', true ); ?>
+		
+		<?php comments_template(); ?>
+		
 	<?php endwhile; ?>
+	
 </main>
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_sidebar(); get_footer();
