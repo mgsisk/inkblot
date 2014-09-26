0bfff26e995c49e13fedd915468643785e3a0d33
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:09:13 -0400

    Refactor search template.
    Removing <hgroup>, simplifying layout, etc.

--- search.php
+++ search.php
@@ -1,41 +1,36 @@
 <?php
-/** Search results template.
+/**
+ * Search results template.
  * 
  * @package Inkblot
+ * @see http://codex.wordpress.org/Template_Hierarchy
  */
 
 get_header(); ?>
 
 <main role="main">
-	<?php if ( have_posts() ) : global $wp_query; ?>
+	
+	<?php if (have_posts()) : ?>
+		
 		<header class="page-header">
-			<hgroup>
-				<h1><?php printf( __( 'Searching for %s', 'inkblot' ), get_search_query() ); ?></h1>
-				<h2><?php printf( __( '%s results', 'inkblot' ), $wp_query->found_posts ); ?></h2>
-			</hgroup>
+			<h1><?php print get_search_query(); ?></h1>
 		</header><!-- .page-header -->
-		<?php inkblot_posts_nav( 'above' ); ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php if ( webcomic() and is_a_webcomic() ) : ?>
-				<?php get_template_part( 'webcomic/content', get_post_type() ); ?>
-			<?php else : ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endif; ?>
-		<?php endwhile; ?>
-		<?php inkblot_posts_nav( 'below' ); ?>
-	<?php else : ?>
-		<header class="page-header">
-			<hgroup>
-				<h1><?php _e( 'Nothing Found', 'inkblot' ); ?></h1>
-				<h2><?php printf( __( 'Searching for %s', 'inkblot' ), get_search_query() ); ?></h2>
-			</hgroup>
-		</header><!-- .page-header -->
-		<div class="page-content">
-			<p><?php _e( 'No results containing all your search terms were found.', 'inkblot' ); ?></p>
-			<?php get_search_form(); ?>
-		</div><!-- .page-content -->
-	<?php endif; ?>
+		
+		<?php
+			while (have_posts()) :
+				the_post();
+				
+				(webcomic() and is_a_webcomic())
+				? get_template_part('webcomic/content', get_post_type())
+				: get_template_part('content', get_post_format());
+			endwhile;
+			
+			print inkblot_posts_nav('below', false, get_theme_mod('paged_navigation', true));
+		else :
+			get_template_part('content', 'none');
+		endif;
+	?>
+	
 </main>
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_sidebar(); get_footer();
