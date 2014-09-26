ef6680de5d66605a5aebef710166df226418b6c9
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:14:39 -0400

    Add full width template.
    Actual just a normal page template; the real magic happens in other
    files.

---
+++ page-templates/full-width.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Full Width
+ * 
+ * Full-width page template.
+ * 
+ * @package Inkblot
+ * @see http://codex.wordpress.org/Template_Hierarchy
+ */
+
+get_header(); ?>
+
+<main role="main">
+	
+	<?php
+		while (have_posts()) :
+			the_post();
+			
+			get_template_part('content', 'page');
+			
+			comments_template();
+		endwhile;
+	?>
+	
+</main>
+
+<?php get_sidebar(); get_footer();
