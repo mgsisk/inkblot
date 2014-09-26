7038c2688a6023e70b12078ecb30c39380d4015f
Michael Sisk <contact@mgsisk.com>
2014-09-26 16:08:46 -0400

    Refactor search form template.

--- searchform.php
+++ searchform.php
@@ -1,12 +1,18 @@
 <?php
-/** Search form template.
+/**
+ * Search form template.
  * 
  * @package Inkblot
+ * @see http://codex.wordpress.org/Function_Reference/get_search_form
  */
 ?>
 
-<form action="<?php echo esc_url( home_url( '/' ) ); ?>" class="search">
-	<label for="<?php inkblot_search_id(); ?>" class="ghost focus"><?php _e( 'Search', 'inkblot' ); ?></label>
-	<input type="search" id="<?php inkblot_search_id( false ); ?>" name="s" placeholder="<?php esc_attr_e( 'Search', 'inkblot' ); ?>">
-	<button type="submit"><?php _e( 'Search', 'inkblot' ); ?></button>
+<form action="<?php print esc_url(home_url('/')); ?>" class="search">
+	<p>
+		<label for="<?php print inkblot_search_id(); ?>"><?php _e('Search', 'inkblot'); ?></label>
+		<input type="search" id="<?php print inkblot_search_id(false); ?>" name="s">
+	</p>
+	<p>
+		<button type="submit"><?php _e('Search', 'inkblot'); ?></button>
+	</p>
 </form><!-- .search -->
