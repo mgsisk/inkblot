e09208d1fdbff8332b7781a07a63b31c56fbed2a
Michael Sisk <me@mgsisk.com>
2015-01-12 13:36:58 -0500

    Add Webcomic feed template.

---
+++ webcomic/feed.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Feed integration template.
+ * 
+ * **NEVER** `print` or `echo` anything in this template. Content you want to
+ * include with your Webcomic posts in site feeds must be assigned to the
+ * `$append` or `$prepend` variables.
+ * 
+ * @package Inkblot
+ */
+$link = get_permalink(get_the_ID());
+$images = '';
+
+foreach ($attachments as $attachment) {
+	$images .= wp_get_attachment_image($attachment->ID, $feed_size);
+}
+
+$prepend = "<p><a href='{$link}'>{$images}</a></p>";
