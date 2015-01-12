0a063e2da1bc5b05db9688ac1a642c5bc3497c7c
Michael Sisk <me@mgsisk.com>
2015-01-12 13:37:27 -0500

    Update Webcomic transcript template.

--- webcomic/transcripts.php
+++ webcomic/transcripts.php
@@ -1,44 +1,45 @@
 <?php
-/** Webcomic transcripts template.
+/**
+ * Webcomic transcripts template.
  * 
- * This template is nearly identical to WordPress' own
- * `comments.php` template, except that we have to check for (and
- * display) transcripts that are Pending Review (i.e. transcripts
- * that users can improve upon).
+ * This template is nearly identical to WordPress' own `comments.php` template,
+ * except that we have to check for (and display) transcripts that are
+ * Pending Review (i.e. transcripts that users can improve upon).
  * 
  * @package Inkblot
  * @see github.com/mgsisk/webcomic/wiki/Templates
  */
 
-global $post;
-?>
+global $post; ?>
 
 <aside id="webcomic-transcripts">
-	<?php if ( post_password_required() ) : ?>
-		
-	<?php else : ?>
-		<?php if ( $transcripts = get_webcomic_transcripts() ) : ?>
-			<header class="transcripts-header">
-				<h1><?php _e( 'Transcripts', 'inkblot' ); ?></h1>
-			</header><!-- .transcripts-header -->
-			<?php foreach ( $transcripts as $post ) : setup_postdata( $post ); ?>
-				<article id="webcomic-transcript-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<div class="post-content">
-						<?php the_content(); ?>
-					</div><!-- .transcript-content -->
-					<footer class="post-footer">
-						<?php printf( __( 'Transcribed by %1$s%2$s', 'inkblot' ), WebcomicTag::get_webcomic_transcript_authors(), WebcomicTag::get_the_webcomic_transcript_term_list( 0, 'webcomic_language', __( ' in ', 'inkblot' ), __( ', ', 'inkblot' ), __( '', 'inkblot' ) ) ); ?>
-					</footer><!-- .transcript-footer -->
-				</article><!-- #webcomic-transcript-<?php the_ID(); ?> -->
-			<?php wp_reset_postdata(); endforeach; ?>
-		<?php elseif ( webcomic_transcripts_open() ) : ?>
+	
+	<?php
+		if (post_password_required()) :
+			// a password is required to view this post
+		else : 
+			if ($transcripts = get_webcomic_transcripts()) : ?>
+				
+				<header class="webcomic-transcripts-header">
+					<h1><?php _e('Transcripts', 'inkblot'); ?></h1>
+				</header><!-- .comments-header -->
+				
+			<?php
+				foreach ($transcripts as $post) : setup_postdata($post);
+					inkblot_webcomic_transcript();
+					
+					wp_reset_postdata();
+				endforeach;
+			endif;
 			
-		<?php endif; ?>
-		<?php if ( webcomic_transcripts_open() and have_webcomic_transcripts( true ) and $transcripts = get_webcomic_transcripts( true ) ) : ?>
-			<?php foreach ( $transcripts as $transcript ) : ?>
-				<?php webcomic_transcript_form( array(), $transcript ); ?>
-			<?php endforeach; ?>
-		<?php endif; ?>
-		<?php webcomic_transcript_form(); ?>
-	<?php endif; ?>
+			if (webcomic_transcripts_open() and $transcripts = get_webcomic_transcripts(true)) :
+				foreach ($transcripts as $transcript) :
+					webcomic_transcript_form(array(), $transcript);
+				endforeach;
+			endif;
+			
+			webcomic_transcript_form();
+		endif;
+	?>
+	
 </aside><!-- #webcomic-transcripts -->
