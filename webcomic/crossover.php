c7c9bdc60679edbec06806b252ad3f0f1d5de3d0
Michael Sisk <me@mgsisk.com>
2015-01-12 13:36:49 -0500

    Split Webcomic archive templates.

---
+++ webcomic/crossover.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Webcomic crossover archive template.
+ * 
+ * @package Inkblot
+ * @see github.com/mgsisk/webcomic/wiki/Templates
+ */
+
+get_header();
+
+if ('ASC' === get_theme_mod('webcomic_archive_order', 'ASC')) :
+	global $wp_query;
+	
+	query_posts(array_merge($wp_query->query_vars, array(
+		'order' => get_theme_mod('webcomic_archive_order', 'ASC')
+	)));
+endif; ?>
+
+<main role="main">
+	
+	<?php if (have_posts()) : ?>
+		
+		<header class="page-header">
+			<h1><?php webcomic_crossover_title(); ?></h1>
+		</header><!-- .page-header -->
+		
+		<?php if (WebcomicTag::webcomic_crossover_image()) : ?>
+			<div class="page-image"><?php webcomic_crossover_image(); ?></div><!-- .page-image -->
+		<?php endif; ?>
+		
+		<?php if (WebcomicTag::webcomic_crossover_description()) : ?>
+			<div class="page-content"><?php webcomic_crossover_description(); ?></div><!-- .page-content -->
+		<?php endif; ?>
+
+		<?php
+			while (have_posts()) :
+				the_post();
+				
+				get_template_part('webcomic/content', get_post_type());
+			endwhile;
+			
+			print inkblot_posts_nav('below', false, get_theme_mod('paged_navigation', true));
+		else:
+			get_template_part('content', 'none');
+		endif;
+	?>
+	
+</main>
+
+<?php get_sidebar(); get_footer();
---
+++ webcomic/storyline.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Webcomic storyline archive template.
+ * 
+ * @package Inkblot
+ * @see github.com/mgsisk/webcomic/wiki/Templates
+ */
+
+get_header();
+
+if ('ASC' === get_theme_mod('webcomic_archive_order', 'ASC')) :
+	global $wp_query;
+	
+	query_posts(array_merge($wp_query->query_vars, array(
+		'order' => get_theme_mod('webcomic_archive_order', 'ASC')
+	)));
+endif; ?>
+
+<main role="main">
+	
+	<?php if (have_posts()) : ?>
+		
+		<header class="page-header">
+			<h1><?php single_term_title(); ?></h1>
+		</header><!-- .page-header -->
+		
+		<?php if (WebcomicTag::webcomic_term_image()) : ?>
+			<div class="page-image"><?php webcomic_storyline_cover(); ?></div><!-- .page-image -->
+		<?php endif; ?>
+		
+		<?php if (term_description()) : ?>
+			<div class="page-content"><?php print term_description(); ?></div><!-- .page-content -->
+		<?php endif; ?>
+		
+		<?php
+			while (have_posts()) :
+				the_post();
+				
+				get_template_part('webcomic/content', get_post_type());
+			endwhile;
+			
+			print inkblot_posts_nav('below', false, get_theme_mod('paged_navigation', true));
+		else:
+			get_template_part('content', 'none');
+		endif;
+	?>
+	
+</main>
+
+<?php get_sidebar(); get_footer();
