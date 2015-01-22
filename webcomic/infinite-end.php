facea87c8b542dd9c358347f530e6710637eadf0
Michael Sisk <me@mgsisk.com>
2015-01-22 15:07:11 -0500

    Clarify template requirements.

--- webcomic/infinite-end.php
+++ webcomic/infinite-end.php
@@ -3,7 +3,7 @@
  * End template for Webcomic Infinite page template.
  * 
  * Loaded at the very end of Webcomic's infinite scroll. You can put whatever
- * you like here, but you at least include an element with a
+ * you like here, but you have to at least include an element with a
  * `data-webcomic-infinite-end` attribute to let Webcomic know we've reached
  * the end of the scroll and can stop checking for more posts.
  * 
