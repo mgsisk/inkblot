18090285c8338522a0ae5acd34f451dbcd75d88d
Michael Sisk <me@mgsisk.com>
2015-04-23 22:21:55 -0400

    Correct sidebar display.
    Handles the full-width template better and supports the widget columns
    option (not that you’d want to use that on the sidebars).

--- sidebar.php
+++ sidebar.php
@@ -5,42 +5,42 @@
  * @package Inkblot
  * @see http://codex.wordpress.org/Template_Hierarchy
  */
+
+if (is_page_template('template/full-width.php') and ! get_post_meta(get_the_ID(), 'inkblot_sidebars', true)) {
+	return;
+}
 ?>
 
-<?php if ( ! is_page_template('template/full-width.php') or get_post_meta(get_the_ID(), 'inkblot_sidebars', true)) : ?>
-
-	<?php if ('one-column' !== get_theme_mod('content', 'one-column') or is_customize_preview()) : ?>
-		
-		<div role="complementary" class="sidebar1 widgets columns-1">
-			
-			<?php if ( ! dynamic_sidebar('primary-sidebar') and current_user_can('edit_theme_options')) : ?>
-				
-				<aside class="widget">
-					<h1><?php _e('Primary Sidebar', 'inkblot'); ?></h1>
-					<p><?php printf(__('This is the primary sidebar. You can remove this message by adding widgets to it from the <a href="%s"><strong>Appearance > Widgets</strong> administrative page</a>.', 'inkblot'), admin_url('widgets.php')); ?></p>
-				</aside>
-				
-			<?php endif; ?>	
-			
-		</div><!-- .sidebar1 -->
-		
-	<?php endif; ?>
+<?php if ('one-column' !== get_theme_mod('content', 'one-column') or is_customize_preview()) : ?>
 	
-	<?php if (false !== strpos(get_theme_mod('content', 'one-column'), 'three-column') or is_customize_preview()) : ?>
+	<div role="complementary" class="sidebar1 widgets columns-<?php print get_theme_mod('sidebar-primary-sidebar-columns', false) ? inkblot_count_widgets('primary-sidebar', 1) : 1; ?>">
 		
-		<div role="complementary" class="sidebar2 widgets columns-1">
+		<?php if ( ! dynamic_sidebar('primary-sidebar') and current_user_can('edit_theme_options')) : ?>
 			
-			<?php if ( ! dynamic_sidebar('secondary-sidebar') and current_user_can('edit_theme_options')) : ?>
-				
-				<aside class="widget">
-					<h1><?php _e('Secondary Sidebar', 'inkblot'); ?></h1>
-					<p><?php printf(__('This is the secondary sidebar. You can remove this message by adding widgets to it from the <a href="%s"><strong>Appearance > Widgets</strong> administrative page</a>.', 'inkblot'), admin_url('widgets.php')); ?></p>
-				</aside>
-				
-			<?php endif; ?>	
+			<aside class="widget">
+				<h1><?php _e('Primary Sidebar', 'inkblot'); ?></h1>
+				<p><?php printf(__('This is the primary sidebar. You can remove this message by adding widgets to it from the <a href="%s">Customizer</a>.', 'inkblot'), admin_url('customize.php')); ?></p>
+			</aside>
 			
-		</div><!-- .sidebar2 -->
+		<?php endif; ?>	
 		
-	<?php endif; ?>
+	</div><!-- .sidebar1 -->
+	
+<?php endif; ?>
+
+<?php if (false !== strpos(get_theme_mod('content', 'one-column'), 'three-column') or is_customize_preview()) : ?>
+	
+	<div role="complementary" class="sidebar2 widgets columns-<?php print get_theme_mod('sidebar-secondary-sidebar-columns', false) ? inkblot_count_widgets('secondary-sidebar', 1) : 1; ?>">
+		
+		<?php if ( ! dynamic_sidebar('secondary-sidebar') and current_user_can('edit_theme_options')) : ?>
+			
+			<aside class="widget">
+				<h1><?php _e('Secondary Sidebar', 'inkblot'); ?></h1>
+				<p><?php printf(__('This is the secondary sidebar. You can remove this message by adding widgets to it from the <a href="%s">Customizer</a>.', 'inkblot'), admin_url('customize.php')); ?></p>
+			</aside>
+			
+		<?php endif; ?>	
+		
+	</div><!-- .sidebar2 -->
 	
 <?php endif; ?>
