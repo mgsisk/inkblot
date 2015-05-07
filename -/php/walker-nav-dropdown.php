e8446e13966dcd74795e3c991de7ffd6dbb09a60
Michael Sisk <me@mgsisk.com>
2015-05-07 15:31:09 -0400

    Update dropdown output.
    Remove unnecessary title attribute, properly escape URL.

--- -/php/walker-nav-dropdown.php
+++ -/php/walker-nav-dropdown.php
@@ -24,16 +24,16 @@ class Inkblot_Walker_Nav_Dropdown extends Walker_Nav_Menu {
 	 * @param object $item Current item being handled by the walker.
 	 * @param integer $depth Depth the walker is currently at.
 	 * @param array $args Arguments passed to the walker.
+	 * @param integer $id ID of the current item.
 	 */
 	function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
 		$classes = empty($item->classes) ? array() : (array) $item->classes;
 		$classes[] = 'menu-item-' . $item->ID;
 		$classes = implode(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
 		
-		$output .= apply_filters('walker_nav_menu_start_el', sprintf('<option value="%s" class="%s" title="%s" data-target="%s"%s>%s%s%s%s',
-			esc_attr($item->url),
+		$output .= apply_filters('walker_nav_menu_start_el', sprintf('<option value="%s" class="%s" data-target="%s"%s>%s%s%s%s',
+			esc_url($item->url),
 			esc_attr($classes),
-			esc_attr($item->attr_title),
 			esc_attr($item->target),
 			selected(false !== strpos($classes, 'current-menu-item'), true, false),
 			str_repeat('&nbsp;', $depth * 4),
